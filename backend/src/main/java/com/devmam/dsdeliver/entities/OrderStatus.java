6ccc17d9c1dd19cc0b708bfe5392d15f64ed76ea
Marcio Andre Mendes <marcioandre.ti1@gmail.com>
2021-12-04 07:47:22 -0300

    GET /orders

--- backend/src/main/java/com/devmam/dsdeliver/entities/OrderStatus.java
+++ backend/src/main/java/com/devmam/dsdeliver/entities/OrderStatus.java
@@ -3,5 +3,8 @@ package com.devmam.dsdeliver.entities;
 public enum OrderStatus {
 	
 	PENDING, DELIVERED;
+	
+	//pending = 0
+	//delivered = 1
 
 }
--- backend/src/main/java/com/devmam/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devmam/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,21 @@
 package com.devmam.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devmam.dsdeliver.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+	//escrevendo a consulta de busca no repository
+	//linguagem jpql
+	//(Order) é o nome da classe e (obj) é apelido para busca
+	
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrdersWithProducts();
+	
 
 }
